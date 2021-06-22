143ac272895f36e48482506fe7a33d4f4bf2025b
Cledson da Silva Araújo <ciebinho.embetant@gmail.com>
2021-06-22 13:51:36 -0300

    adding state to Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Button} from "./components/Button"
 
 function App() {
   return (
-    <Button>Ola NLW</Button>
+    <Button></Button>
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,16 @@
-type ButtonProps = {
-	children?: string
-};
+import { useState } from "react";
+// type ButtonProps = {
+// 	children?: string
+// };
 //obs: se for ultilizado text?: string; isso indica que
 //a propriedade text é opcional
-export function Button(props: ButtonProps) {
-	return <button>{props.children|| 'Default'}</button>;
+// props: ButtonProps
+export function Button() {
+	const [counter,setCounter] = useState(0)
+	function increment(){
+		setCounter(counter + 1);
+	}
+	return <button onClick={increment}>{counter}</button>;
 	//obs: o {props.text || "Default"} indica que caso a
 	//props.text não seja passada (já que ela é opcinal)
 	//o Default values o substitui
