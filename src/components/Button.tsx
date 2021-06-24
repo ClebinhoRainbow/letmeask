5f2ec8b1cb443382f03ceb1d0466088102654d94
Cledson da Silva Araújo <ciebinho.embetant@gmail.com>
2021-06-24 13:22:38 -0300

    creating Home.tsx and NewRoom.tsx with authentication

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-// type ButtonProps = {
-// 	children?: string
-// };
-//obs: se for ultilizado text?: string; isso indica que
-//a propriedade text é opcional
-// props: ButtonProps
-export function Button() {
-	const [counter,setCounter] = useState(0)
-	function increment(){
-		setCounter(counter + 1);
-	}
-	return <button onClick={increment}>{counter}</button>;
-	//obs: o {props.text || "Default"} indica que caso a
-	//props.text não seja passada (já que ela é opcinal)
-	//o Default values o substitui
+import { ButtonHTMLAttributes } from "react";
+import  "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+export function Button(props: ButtonProps ) {
+	
+	return <button className="button" {...props}/>
 }
