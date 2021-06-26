9c5186968e543d6820265287beca4d8ac3b2ed45
Cledson da Silva Araújo <ciebinho.embetant@gmail.com>
2021-06-26 10:23:58 -0300

    completing deleting questions and rooms

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,10 @@
 import { ButtonHTMLAttributes } from "react";
 import  "../styles/button.scss";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
-export function Button(props: ButtonProps ) {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	isOutLined?: boolean;
+};
+export function Button({isOutLined = false, ...props}: ButtonProps ) {
 	
-	return <button className="button" {...props}/>
+	return <button className={`button ${isOutLined ? 'outlined': ''}`} {...props}/>
 }
